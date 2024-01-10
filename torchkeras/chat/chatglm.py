c6f566303a4258d6c81cf6437f49a616db63e818
zebo <112797813+AAA34WEt@users.noreply.github.com>
2024-01-10 11:33:38 +0800

    Update chatglm.py

--- torchkeras/chat/chatglm.py
+++ torchkeras/chat/chatglm.py
@@ -10,8 +10,8 @@ class ChatGLM(object):
                  num_beams=1,
                  do_sample=True,
                  temperature=0.8,
-                 logits_processor=None,
                  top_p=0.9,
+                 logits_processor=None
                 ):
         
         self.__dict__.update(locals())
@@ -37,21 +37,21 @@ class ChatGLM(object):
         
         if not self.stream:
             response, self.history = self.model.chat(self.tokenizer,
-                                                     query,self.history,self.max_length,self.num_beams,
-                                                     self.do_sample,self.top_p,self.temperature,
-                                                     self.logits_processor)
+                query,self.history,self.max_length,self.num_beams,
+                self.do_sample,self.top_p,self.temperature,
+                self.logits_processor)
             return response 
         
         result = self.model.stream_chat(self.tokenizer,
-                                        query,self.history,None,self.max_length,
-                                        self.do_sample,self.top_p,self.temperature,
-                                        self.logits_processor,None)
+                query,self.history,None,self.max_length,
+                self.do_sample,self.top_p,self.temperature,
+                self.logits_processor,None)
         
         for response,history in result:
             print(response)
             clear_output(wait=True)
-            self.history = history
-        #clear_output(wait=True)
+        self.history = history
+        #clear_output(waite=True)
         return response
     
     def register_magic(self):
